ec584f109e88d3143ad678da994b0d6bc02182ce
fabiankramm <kramm@covexo.com>
2020-04-17 14:24:29 +0200

    small improvements & fixes

--- pkg/util/clienthelper/helper.go
+++ pkg/util/clienthelper/helper.go
@@ -2,17 +2,22 @@ package clienthelper
 
 import (
 	"context"
+	"io/ioutil"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/klog"
-	"os"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-func CurrentNamespace() string {
-	return os.Getenv("NAMESPACE")
+func CurrentNamespace() (string, error) {
+	namespace, err := ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace")
+	if err != nil {
+		return "", err
+	}
+
+	return string(namespace), nil
 }
 
 func CreateWithOwner(ctx context.Context, client client.Client, object runtime.Object, owner metav1.Object, scheme *runtime.Scheme) error {
